e2df4d65253e6ebd44b50e0eb5272160bbd82493
Carlos <carlos_arellano@hotmail.ca>
2019-08-11 17:23:50 -0400

    created personal bio and projects descriptions

--- src/Home.js
+++ src/Home.js
@@ -9,17 +9,19 @@ const Home = () => {
 		<h1>Carlos Arellano</h1>
 		<div className='bio animated fadeInRight'>
 			<h2>About me</h2>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-			Consequuntur, modi. Repellendus labore tempore, libero, 
-			natus at reiciendis nemo exercitationem assumenda alias 
-			cupiditate inventore pariatur veritatis repudiandae magnam, 
-			ex dolore. Necessitatibus.
+      <p>Hey there, my name is Carlos Arellano. A FrontEnd Web Developer 
+		  recently graduated from the Web Design and Development program at humber. 
+			I began my journey in the Developing world almost 2 years ago when I was 
+			enrolled in a program at Sheridan College. In the last couple of years I have 
+			become quite passionate for creating experiences using different tools and colours 
+			for the people using my websites. 
 			</p>
 			<p>
-			Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-			Magnam doloribus, optio dolorum dolorem sint, voluptatibus 
-			aut at hic, laboriosam omnis doloremque modi voluptas repellat
-			 culpa porro et animi incidunt consequuntur.
+			I consider myself someone that is a curious person, always looking to find new 
+			ways to do things and learn new ways to solve an issue.
+			In the time that I have been involved in the technology field, I have been able to 
+			learn multiple languages like Javascript, HTML and CSS, also frameworks such as 
+			ReactJS, NodeJS and CSS frameworks like Bootstrap and Sass.
 			</p>
 
 			<div>
--- src/Work.js
+++ src/Work.js
@@ -14,11 +14,41 @@ const Work = (  ) => {
 				<a href="http://nowplaying.arellanodesigns.ca/" target= '_blank' rel='noreferrer noopener'>
 				<img src= {nowPlaying} alt= 'Now Playing project preview' className= 'nowPlaying' id= 'nowPlaying' />
 				</a>
-				<p className= 'about-site'>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-			Consequuntur, modi. Repellendus labore tempore, libero, 
-			natus at reiciendis nemo exercitationem assumenda alias 
-			cupiditate inventore pariatur veritatis repudiandae magnam, 
-			ex dolore. Necessitatibus.</p>
+				<p className= 'about-site'>
+					<h2>nowPlaying</h2>
+				Now playing is a responsive website that allows the user to find the latest 
+				movies in their area, it also displays information such as a current rating, release date, synopsis and an 
+				official trailer.
+				<ul>
+					<li>HTML</li>
+					<li>CSS</li>
+					<li>Javascript</li>
+					<li>API</li>
+					<li>Animate.css</li>
+				</ul>
+				</p>
+			</div>
+
+			<div className= 'project animated fadeInRight'>
+				<a href="http://nowplaying.arellanodesigns.ca/" target= '_blank' rel='noreferrer noopener'>
+				<img src= {rbnb} alt= 'Breakfast&Chill project preview' className= 'nowPlaying' id= 'nowPlaying' />
+				</a>
+				<p className= 'about-site'>
+					<h2>Breakfast&Chill</h2>
+					Breakfast&Chill is a website that allows the user to explore places to rent 
+					in some of the major cities in the world. The user is also able to create a user 
+					account and filter rooms based in the city of their interest. Text message and
+					email confirmation will be sent to the user when creating an account. 
+					<ul>
+						<li>NodeJS</li>
+						<li>MongoDB</li>
+						<li>Mongoose</li>
+						<li>Express</li>
+						<li>Handlebars</li>
+						<li>Animate.css</li>
+						<li>Twilio</li>
+					</ul>
+					</p>
 			</div>
 
 			<div className= 'project animated fadeInRight'>
@@ -32,16 +62,6 @@ const Work = (  ) => {
 			ex dolore. Necessitatibus.</p>
 			</div>
 
-			<div className= 'project animated fadeInRight'>
-				<a href="http://nowplaying.arellanodesigns.ca/" target= '_blank' rel='noreferrer noopener'>
-				<img src= {rbnb} alt= 'Now Playing project preview' className= 'nowPlaying' id= 'nowPlaying' />
-				</a>
-				<p className= 'about-site'>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-			Consequuntur, modi. Repellendus labore tempore, libero, 
-			natus at reiciendis nemo exercitationem assumenda alias 
-			cupiditate inventore pariatur veritatis repudiandae magnam, 
-			ex dolore. Necessitatibus.</p>
-			</div>
 		</div>
 	)
 }
