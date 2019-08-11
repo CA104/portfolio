4db619c5a06f0c8c291361e691395b7aad3cf03b
Carlos <carlos_arellano@hotmail.ca>
2019-08-11 15:33:10 -0400

    added resume button, adjusted styling on all pages

--- src/Contact.js
+++ src/Contact.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import linkedin from './img/linkedin.png'
 import git from './img/github-sign.png'
+import resume from './img/Resume.pdf'
 
 const Contact = (  ) => {
 
@@ -11,8 +12,9 @@ const Contact = (  ) => {
 			</h1>
 
 			<div className= 'contact animated fadeInRight'>
-				<a href= 'mailto:carlos_arellano@hotmail.ca'>carlos_arellano@hotmail.ca</a>
+				<a href= 'mailto:carlos_arellano@hotmail.ca' className= 'info'>carlos_arellano@hotmail.ca</a>
 				<p>[647] 986-4001</p>
+				<a href={resume} download= '' className= 'info'>My Resume</a>
 				<div>
 				<a href="https://www.linkedin.com/in/carlos-arellano-43a530105/" target= '_blank' rel='noreferrer noopener' >
 				<img id= 'linkedIn' src= {linkedin} alt="" className= 'linkedin' />
@@ -20,6 +22,10 @@ const Contact = (  ) => {
 				<a href="https://github.com/" target= '_blank' rel='noreferrer noopener' >
 				<img id= 'gitHub' src= {git} alt="" className= 'git' />
 				</a>
+			</div>
+
+			<div>
+				<form action=""></form>
 			</div>
 			</div>
 		</div>
